25821f03eb6b88defa0f802d3ebcf75ff7ececfa
erdzeichen <jensuhlig0@gmail.com>
2022-03-24 19:44:54 +0100

    no message

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 
-
-sys.path.insert(0, os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath('../../'))
+package_path = os.path.abspath('../..')
+os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 
 
