04b7a7deda76530498868c77dc77f1a592987366
Jens Uhlig <jensuhlig0@gmail.com>
2022-03-24 16:38:15 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..\\..\\code_folder'))
+sys.path.insert(0, os.path.abspath('..\\code_folder'))
 
 
 # -- Project information -----------------------------------------------------
